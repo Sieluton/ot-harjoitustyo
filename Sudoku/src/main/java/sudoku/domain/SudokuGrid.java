f29660146d45a10523be5f446c3fd45cab06078f
Sami <sami.rinksam@gmail.com>
2018-11-20 23:39:37 +0200

    Testit lisätty ja hieman lisää pelin logiikkaa

--- Sudoku/src/main/java/sudoku/domain/SudokuGrid.java
+++ Sudoku/src/main/java/sudoku/domain/SudokuGrid.java
@@ -3,8 +3,10 @@ package sudoku.domain;
 public class SudokuGrid {
     int[][] grid;
 
+    // Creates empty grid if input is not accepted by regex
     public SudokuGrid(String numbers){
         grid = new int[9][9];
+        // if input is 81 characters long and contains only numbers from 0-9 it is made
         if (numbers.matches("[0-9]{81}")) {
             int next = 0;
             for (int y = 0; y < 9; y++) {
@@ -16,8 +18,35 @@ public class SudokuGrid {
         }
     }
 
+    // Returns whole grid
     public int[][] getGrid(){
         return grid;
+    }
+
+    // Sets specific number in grid
+    public void setNumber(int y, int x, int number){
+        grid[y][x] = number;
+    }
+
+    // Returns specific number from grid
+    public int getNumber(int y, int x){
+        return grid[y][x];
+    }
+
+    // Checks if row contains only one of each number (1-9)
+    public boolean checkRow(int y){
+        for (int x  = 0; x < 9; x++){
+
+        }
+        return true;
+    }
+
+    // Checks if column contains only one of each number (1-9)
+    public boolean checkColumn(int x){
+        for (int y  = 0; y < 9; y++){
+
+        }
+        return true;
     }
 
 }
---
+++ Sudoku/src/test/java/sudoku/domain/SudokuGridTest.java
@@ -0,0 +1,38 @@
+package sudoku.domain;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+import java.util.*;
+
+public class SudokuGridTest {
+
+    @Test
+    public void equalWhenConstructorGivenWrongInput(){
+        int[][] grid1 = new SudokuGrid("").getGrid();
+        int[] grid2 = new int[]{0,0,0,0,0,0,0,0,0};
+        for (int i = 0; i < 9; i++){
+            assertTrue(Arrays.equals(grid1[i],grid2));
+        }
+    }
+
+    @Test
+    public void equalWhenConstructorGivenValidInput(){
+        int[][] grid1 = new SudokuGrid("295743861431865927876192543389459216612387495549216738763534189928671354154938672").getGrid();
+        int[][] grid2 = new int[][]{
+                {2,9,5,7,4,3,8,6,1},
+                {4,3,1,8,6,5,9,2,7},
+                {8,7,6,1,9,2,5,4,3},
+                {3,8,9,4,5,9,2,1,6},
+                {6,1,2,3,8,7,4,9,5},
+                {5,4,9,2,1,6,7,3,8},
+                {7,6,3,5,3,4,1,8,9},
+                {9,2,8,6,7,1,3,5,4},
+                {1,5,4,9,3,8,6,7,2},
+        };
+        for (int i = 0; i < 9; i++){
+            assertTrue(Arrays.equals(grid1[i],grid2[i]));
+        }
+    }
+
+}
