ac5be9ca99ff060db5c517f69e63f73db580b227
Sami <sami.rinksam@gmail.com>
2018-11-27 23:34:52 +0200

    Saadaan sudoku ruudukko piirrettyä

--- Sudoku/src/main/java/sudoku/ui/SudokuUi.java
+++ Sudoku/src/main/java/sudoku/ui/SudokuUi.java
@@ -1,5 +1,4 @@
 package sudoku.ui;
-// 295743861431865927876192543389459216612387495549216738763534189928671354154938672
 import javafx.application.Application;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
@@ -7,13 +6,15 @@ import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.layout.StackPane;
 import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
 import javafx.stage.Stage;
+import sudoku.domain.SudokuGrid;
 
 import java.util.*;
 
 public class SudokuUi extends Application {
-    private static final int RECT_HEIGHT = 30;
-    private static final int RECT_WIDTH = 30;
+    public SudokuGrid sudokuGrid = new SudokuGrid("295743861431865927876192543389459216612387495549216738763534189928671354154938672");
+    public Canvas canvas;
 
     public static void main(String[] args) {
         launch(args);
@@ -23,7 +24,7 @@ public class SudokuUi extends Application {
     public void start(Stage primaryStage) throws Exception {
         primaryStage.setTitle("Sudoku");
         StackPane layout = new StackPane();
-        Canvas canvas = new Canvas(378, 378);
+        canvas = new Canvas(378, 378);
         layout.getChildren().add(canvas);
         Scene scene = new Scene(layout, 400,400, Color.GREY);
         GraphicsContext gc = canvas.getGraphicsContext2D();
@@ -38,12 +39,17 @@ public class SudokuUi extends Application {
         gc.clearRect(0,0,378,378);
         for(int row = 0; row<9; row++) {
             for(int col = 0; col<9; col++) {
-                int position_y = row * 40 + 2;
-                int position_x = col * 40 + 2;
+                int y = row * 40 + 2;
+                int x = col * 40 + 2;
                 int width = 36;
 
                 gc.setFill(Color.LIGHTGRAY);
-                gc.fillRoundRect(position_x, position_y, width, width, 10, 10);
+                gc.fillRoundRect(x, y, width, width, 10, 10);
+                gc.setFill(Color.BLACK);
+                gc.setFont(new Font(20));
+                if (sudokuGrid.getNumber(row, col) != 0) {
+                    gc.fillText(sudokuGrid.getNumber(row, col) + "",x+13,y+25);
+                }
             }
         }
     }
